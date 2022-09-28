436e0584cd5c48e87443ddad18656be81571e9d3
JhonatanDavidMottaMedinaADSO <jhonatanmotta07@gmail.com>
2022-09-28 10:50:51 -0500

    Ejercicio 1

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 addEventListener("DOMContentLoaded", ()=>{
     // 1. Escribir un programa que sume, reste, multiplique y divida dos números
     
-    let num1 = Number(prompt("Ingrese el numero 1", 2));
-    let num2 = Number(prompt("Ingrese el numero 2", 2));
+    // 1. Se requiere calcular la distancia entre dos puntos, realizar el código que permita hallar la solución,
+    // por favor utilizar funciones matemáticas de Python para ello d = rc((x2-x1)^2 + (y2-y1)^2)
+    
+    let x1 =  Number(prompt(`Ingrese el valor de x1:`))
+    let x2 =  Number(prompt(`Ingrese el valor de x2:`))
+    let y1 =  Number(prompt(`Ingrese el valor de y1:`))
+    let y2 =  Number(prompt(`Ingrese el valor de y2:`))
 
-    console.log(`La suma es ${num1} + ${num2} = ${num1 + num2}`);
-    console.log(`La resta es ${num1} - ${num2} = ${num1 - num2}`);
-    console.log(`La multiplicacion es ${num1} * ${num2} = ${num1 * num2}`);
-    console.log(`La division es ${num1} / ${num2} = ${num1 / num2}`);
-
-
-
-
-
+   
+    let d = Math.sqrt(Math.pow((x2)-(x1),2) + Math.pow((y2)-(y1),2))
 
 
+    console.log(`%cLa distancia entre dos puntos es ${d}`, `background-color: yellow; border-radius: 2px; border: 4px groove orange; color: black;`)
 })
