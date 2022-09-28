48b61d690d0daa1aff76d58cd692ac0a5af7bbff
JhonatanDavidMottaMedinaADSO <jhonatanmotta07@gmail.com>
2022-09-28 11:24:07 -0500

    Ejercicio 2

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 addEventListener("DOMContentLoaded", ()=>{
     // 1. Escribir un programa que sume, reste, multiplique y divida dos números
     
-    // 1. Se requiere calcular la distancia entre dos puntos, realizar el código que permita hallar la solución,
-    // por favor utilizar funciones matemáticas de Python para ello d = rc((x2-x1)^2 + (y2-y1)^2)
+    // 2. Pablo compro 5 productos los dos primeros con un 5% de descuento y los 2 últimos con un 2% de
+    // descuento, realice un algoritmo que indique cuanto fue el valor a pagar por cada producto y el total de la compra.
     
-    let x1 =  Number(prompt(`Ingrese el valor de x1:`))
-    let x2 =  Number(prompt(`Ingrese el valor de x2:`))
-    let y1 =  Number(prompt(`Ingrese el valor de y1:`))
-    let y2 =  Number(prompt(`Ingrese el valor de y2:`))
+    let x1 =  Number(prompt(`Valor del producto 1`))
+    let x2 =  Number(prompt(`Valor del producto 2`))
+    let x3 =  Number(prompt(`Valor del producto 3`))
+    let x4 =  Number(prompt(`Valor del producto 4`))
+    let x5 =  Number(prompt(`Valor del producto 5`))
 
-   
-    let d = Math.sqrt(Math.pow((x2)-(x1),2) + Math.pow((y2)-(y1),2))
+    let desx1 = x1 - ((x1) * 0.05)
 
+    let desx2 = x2 - ((x2) * 0.05)
+ 
+    let desx3 = x3 - ((x3) * 0.02)
 
-    console.log(`%cLa distancia entre dos puntos es ${d}`, `background-color: yellow; border-radius: 2px; border: 4px groove orange; color: black;`)
+    let desx4 = x4 - ((x4) * 0.02)
+
+    let total = desx1 + desx2 + desx3 + desx4 + x5
+
+    console.log(`%cEl precio del producto 1 es: ${desx1}
+El precio del producto 2 es: ${desx2}
+El precio del producto 3 es: ${desx3}
+El precio del producto 4 es: ${desx4}
+El precio del producto 5 es: ${x5}
+El total es: ${total}`, `background-color: yellow; border-radius: 2px; border: 4px groove orange; color: black;`)
 })
