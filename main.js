b15233729de09294ef13415cf47f05c57419f38d
JhonatanDavidMottaMedinaADSO <jhonatanmotta07@gmail.com>
2022-09-28 13:00:42 -0500

    Ejercicio 10

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
 addEventListener("DOMContentLoaded", ()=>{
     
-    // Una empresa que contrata personal requiere determinar la edad de las personas que solicitan trabajo,
-    // pero cuando se les realiza la entrevista sólo se les pregunta el año en que nacieron. Realice el código
-//     // que representen el algoritmo para solucionar este problema.
-//     añon=int(input("Ingrese el año de nacimiento: "))
-//     añoa=int(input("Ingrese el año actual: "))
-//     edad=añoa-añon
-//     print("La edad es", edad)
+    // 10. Ingrese una frase que lo identifique como programador de Software y luego mostar esta frase
+    // invertida.
 
-    let añoNa = Number(prompt(`Ingrese su año de nacimiento`))
-    let añoAct = Number(prompt(`Ingrese el año actual`))
-
-    let edad = añoAct - añoNa
-
-    console.log(`%cLa edad es ${edad}`, `background-color: yellow; border-radius: 2px; border: 4px groove orange; color: black;`)
+    let frase = (prompt(`Ingrese la frase a invertir`))
+    console.log(`%c${frase}`, `background-color: yellow; border-radius: 2px; border: 4px groove orange; color: black;`)
+    
+    let fraseInvert = frase.split("").reverse().join("")
+    console.log(`%c${fraseInvert}`, `background-color: yellow; border-radius: 2px; border: 4px groove orange; color: black;`)
 })
